914ab13f9689f9e784369ca44c697c7c668ca4d8
dominik.mirek <dominik.mirek@hotmail.com>
2023-10-17 11:58:43 +0200

    jgkvgu

---
+++ app/src/main/java/com/example/myapplication/SlidersItam.java
@@ -0,0 +1,12 @@
+package com.example.myapplication;
+
+public class SlidersItam {
+    private int image;
+    SlidersItam(int image){
+        this.image = image;
+    }
+    public int getImage(){
+        return image;
+    }
+
+}
